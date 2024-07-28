f2244a9d19f10717b7eee69f5fc633617b7b3dc3
salamandermartin <salamancam.programming@gmail.com>
2024-07-28 13:45:59 -0400

    element position changes

---
+++ scripts/sticky_nav.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {navbarVis()}
+
+var navbar = document.getElementById("nav_sticky");
+
+var sticky = navbar.offsetTop;
+
+function navbarVis(){
+    if (window.scrollY >= sticky){
+        navbar.classList.add("sticky")
+        navbar.style.visibility = "visible";
+    }
+    else {
+        navbar.classList.remove("sticky");
+        navbar.style.visibility = "hidden";
+        // navbar.style.display = "none";
+    }
+}
