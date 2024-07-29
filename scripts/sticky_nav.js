755ab1ab6e5a2a1c98fbc170161f096c374fdd14
salamandermartin <salamancam.programming@gmail.com>
2024-07-29 05:58:25 -0400

    style changes

--- scripts/sticky_nav.js
+++ scripts/sticky_nav.js
@@ -7,11 +7,11 @@ var sticky = navbar.offsetTop;
 function navbarVis(){
     if (window.scrollY >= sticky){
         navbar.classList.add("sticky")
-        navbar.style.visibility = "visible";
+        // navbar.style.visibility = "visible";
     }
     else {
         navbar.classList.remove("sticky");
-        navbar.style.visibility = "hidden";
+        // navbar.style.visibility = "hidden";
         // navbar.style.display = "none";
     }
 }
